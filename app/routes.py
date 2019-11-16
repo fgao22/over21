3b87f1ce613f044809b1d08dbe2cb47f427c4490
Fei Gao <fgao22@live.com>
2019-11-16 17:47:50 -0500

    move stuff around

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,48 @@
+from flask import json
+
 from app import app
+from pyfingerprint.pyfingerprint import PyFingerprint
+
 
 @app.route('/')
-@app.route('/index')
+@app.route('/health')
 def index():
-    return "Hello, World!"
+    try:
+        f = PyFingerprint('/dev/ttyUSB0', 57600, 0xFFFFFFFF, 0x00000000)
+
+        if (f.verifyPassword() == False):
+            raise ValueError('The given fingerprint sensor password is wrong!')
+
+    except Exception as e:
+        print('The fingerprint sensor could not be initialized!')
+        print('Exception message: ' + str(e))
+        return app.response_class(
+            response=json.dumps(data),
+            status=500,
+            mimetype='application/json'
+        )
+
+    # Gets some sensor information
+    print('Currently used templates: ' + str(f.getTemplateCount()) +
+          '/' + str(f.getStorageCapacity()))
+
+    # Tries to show a template index table page
+    try:
+        page = input(
+            'Please enter the index page (0, 1, 2, 3) you want to see: ')
+        page = int(page)
+
+        tableIndex = f.getTemplateIndex(page)
+
+        for i in range(0, len(tableIndex)):
+            print('Template at position #' + str(i) +
+                  ' is used: ' + str(tableIndex[i]))
+
+    except Exception as e:
+        print('Operation failed!')
+        print('Exception message: ' + str(e))
+        return app.response_class(
+            response=json.dumps(data),
+            status=501,
+            mimetype='application/json'
+        )
